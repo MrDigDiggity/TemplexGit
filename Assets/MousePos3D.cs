03d8e61359f175e527a298482cefd5ab79d27b37
MrDigDiggity <94404513+MrDigDiggity@users.noreply.github.com>
2022-05-23 14:31:41 +1200

    introduced a way to figure out mouse pos
    This will be later used for adjusting the characters standing direction

---
+++ Assets/MousePos3D.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MousePos3D : MonoBehaviour
+{
+    [SerializeField] private Camera mainCamera;
+
+    private void Update()
+    {
+        Ray rayb = mainCamera.ScreenPointToRay(Input.mousePosition);
+         if (Physics.Raycast(rayb, out RaycastHit raycastHit))
+         {
+             transform.position = raycastHit.point;
+         }
+    }
+}
